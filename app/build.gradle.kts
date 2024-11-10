64a83a06aa1f74762368759d3fa3f02bbe75a579
Manish <man112056@gmail.com>
2024-11-11 00:51:28 +0530

    build fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.manish.sampledesignpattern"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.manish.sampledesignpattern"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
